a2998250bf55c46d3ca48bfa9118ed8e122298c6
Hugo Camboulive <hugo.camboulive@gmail.com>
2009-01-11 17:59:51 +0100

    Add a function to transform channels into raw data. Add doxygen documentation to the channel functions.

--- channel.c
+++ channel.c
@@ -3,16 +3,38 @@
 #include <string.h>
 #include <stdio.h>
 
-
-int destroy_channel(/*struct server * chanman, */struct channel * chan)
+/**
+ * Destroys a channel and all its fields
+ *
+ * @param chan the channel we are going to destroy
+ *
+ * @return 1 on success
+ */
+int destroy_channel(struct channel * chan)
 {
 	/* TODO : relocate players to another channel maybe? */
-	
+	free(chan->name);
+	free(chan->topic);
+	free(chan->desc);
+	free(chan->players);
+
 	free(chan);
 	return 1;
 }
 
-/** Initialize a new channel */
+/**
+ * Initialize a new channel.
+ * 
+ * @param name the name of the channel
+ * @param topic the topic of the channel
+ * @param desc the description of the channel
+ * @param flags the flags of the channel in a bitfield (see channel.h for values)
+ * @param codec the codec of the channel (see channel.h for values)
+ * @param sort_order the sort order of the channel. No idea what it is
+ * @param max_users the maximum number of users in the channel
+ *
+ * @return the newly allocated and initialized channel
+ */
 struct channel * 
 new_channel(char * name, char * topic, char * desc, uint16_t flags, uint16_t codec, uint16_t sort_order, uint16_t max_users)
 {
@@ -34,13 +56,23 @@ new_channel(char * name, char * topic, char * desc, uint16_t flags, uint16_t cod
 	return chan;
 };
 
-/** Initialize a default channel for testing */
+/**
+ * Initialize a default channel for testing.
+ * This will not give a channel it's ID (determined at insertion)
+ *
+ * @return a test channel.
+ */
 struct channel * new_predef_channel()
 {
 	uint16_t flags = CHANNEL_FLAG_REGISTERED;
 	return new_channel("Channel name", "Channel topic", "Channel description", flags, CODEC_SPEEX_19_6, 0, 16);
 }
 
+
+/**
+ * Prints a channel's information.
+ * @param chan a channel
+ */
 void print_channel(struct channel * chan)
 {
 	if(chan == NULL) {
@@ -55,6 +87,16 @@ void print_channel(struct channel * chan)
 	}
 }
 
+/**
+ * Add a player to a channel (both have to exist).
+ * This will NOT remove the player from his existing channel if
+ * he has one.
+ * @param chan an existing channel
+ * @param pl an existing player
+ *
+ * @return 1 if the insertion succeeded, 
+ * 	   0 if it failed (maximum number of users in channel already reached
+ */
 int add_player_to_channel(struct channel * chan, struct player * pl)
 {
 	int i;
@@ -68,5 +110,40 @@ int add_player_to_channel(struct channel * chan, struct player * pl)
 		}
 		return 1;
 	}
+	chan->current_users--;
+	pl->in_chan = NULL;
 	return 0;
+}
+
+/**
+ * Converts a channel to a data block that can be sent
+ * over the network.
+ *
+ * @param ch the channel we are going to convert to a data block
+ * @param data a pointer to an unallocated data block
+ *
+ * @return the number of bytes that have been allocated/written
+ */
+int channel_to_data(struct channel *ch, char **data)
+{
+	int size = 4 + 2 + 2 + 4 + 2 + 2
+	        + strlen(ch->name) +1
+		+ strlen(ch->topic) +1
+		+ strlen(ch->desc) +1;
+	char *ptr;
+
+	*data = (char *)calloc(size, sizeof(char));
+	ptr = *data;
+
+	*(uint32_t *)ptr = ch->id;		ptr += 4;
+	*(uint16_t *)ptr = ch->flags;		ptr += 2;
+	*(uint16_t *)ptr = ch->codec;		ptr += 2;
+	*(uint32_t *)ptr = 0xFFFFFFFF;		ptr += 4;
+	*(uint16_t *)ptr = ch->sort_order;	ptr += 2;
+	*(uint16_t *)ptr = max_users;		ptr += 2;
+	strcpy(ptr, ch->name);			ptr += (strlen(ch->name) +1);
+	strcpy(ptr, ch->topic);			ptr += (strlen(ch->topic) +1);
+	strcpy(ptr, ch->desc);			ptr += (strlen(ch->desc) +1);
+
+	return size;
 }
