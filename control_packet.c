fe66513f15be80409bf32ac2999e5f54662f6737
Hugo Camboulive <hugo.camboulive@gmail.com>
2009-01-26 23:12:22 +0100

    Add callback to unban IPs.

--- control_packet.c
+++ control_packet.c
@@ -764,6 +764,34 @@ void *c_req_list_bans(char *data, unsigned int len, struct sockaddr_in *cli_addr
 		send_acknowledge(pl);		/* ACK */
 		s_resp_bans(ts_server, pl);
 	}
-	printf("LIST OF BANS\n");
+	return NULL;
+}
+
+/**
+ * Handles a request to remove a ban.
+ *
+ * @param data the request packet
+ * @param len the length of data
+ * @param cli_addr the address of the sender
+ * @param cli_len the length of cli_addr
+ */
+void *c_req_remove_ban(char *data, unsigned int len, struct sockaddr_in *cli_addr, unsigned int cli_len)
+{
+	uint32_t pub_id, priv_id;
+	struct player *pl;
+	struct in_addr ip;
+	struct ban *b;
+
+	memcpy(&priv_id, data + 4, 4);
+	memcpy(&pub_id, data + 8, 4);
+	pl = get_player_by_ids(ts_server, pub_id, priv_id);
+
+	if (pl != NULL && pl->global_flags & GLOBAL_FLAG_SERVERADMIN) {
+		send_acknowledge(pl);		/* ACK */
+		inet_aton(data+24, &ip);
+		b = get_ban_by_ip(ts_server, ip);
+		if (b != NULL)
+			remove_ban(ts_server, b);
+	}
 	return NULL;
 }
--- main_serv.c
+++ main_serv.c
@@ -53,11 +53,12 @@ static void test_init_server()
 static void init_callbacks()
 {
 	bzero(f0_callbacks, 255 * sizeof(packet_function));
-	f0_callbacks[0x45] = &c_req_ban;		/* client wants to ban someone */
 	f0_callbacks[0x2c] = &c_req_leave;		/* client wants to leave */
 	f0_callbacks[0x2d] = &c_req_kick_server;	/* client wants to kick someone from the server */
 	f0_callbacks[0x2e] = &c_req_kick_channel;	/* client wants to kick someone from the channel */
 	f0_callbacks[0x2f] = &c_req_switch_channel;	/* client wants to switch channels */
+	f0_callbacks[0x45] = &c_req_ban;		/* client wants to ban someone */
+	f0_callbacks[0x46] = &c_req_remove_ban;		/* client wants to unban someone */
 	f0_callbacks[0x9a] = &c_req_list_bans;		/* client wants the list of bans */
 	//f4be0300
 	/* callbacks[0] = myfunc1; ... */
